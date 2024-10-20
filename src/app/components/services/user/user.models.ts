d35802f1d0920f3d8a214c408d1f673ea72842d0
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-10-20 00:27:22 -0600

    deleted:    src/app/components/services/account/account.models.ts 	deleted:    src/app/components/services/account/account.service.spec.ts 	deleted:    src/app/components/services/account/account.service.ts 	deleted:    src/app/components/services/auth/auth.models.ts 	deleted:    src/app/components/services/auth/auth.service.spec.ts 	deleted:    src/app/components/services/auth/auth.service.ts 	deleted:    src/app/components/services/csrf/csrf.service.spec.ts 	deleted:    src/app/components/services/csrf/csrf.service.ts 	deleted:    src/app/components/services/password/password.service.spec.ts 	deleted:    src/app/components/services/password/password.service.ts 	deleted:    src/app/components/services/theme.service.spec.ts 	deleted:    src/app/components/services/theme.service.ts 	deleted:    src/app/components/services/user/user.models.ts 	deleted:    src/app/components/services/user/user.service.spec.ts 	deleted:    src/app/components/services/user/user.service.ts 	deleted:    src/app/components/shared/footer/footer.component.html 	deleted:    src/app/components/shared/footer/footer.component.scss 	deleted:    src/app/components/shared/footer/footer.component.spec.ts 	deleted:    src/app/components/shared/footer/footer.component.ts 	deleted:    src/app/components/shared/header/header.component.html 	deleted:    src/app/components/shared/header/header.component.scss 	deleted:    src/app/components/shared/header/header.component.spec.ts 	deleted:    src/app/components/shared/header/header.component.ts

--- src/app/components/services/user/user.models.ts
+++
@@ -1,9 +0,0 @@
-export interface Usuario {
-    id: number;
-    nombre: string;
-    apellido_paterno: string;
-    apellido_materno: string;
-    email: string;
-    telefono: string;
-    rol_id: number;
-}
