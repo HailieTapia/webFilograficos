839556e43b1118d3f7ce0b925e74b82e1513d6e2
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-10-20 16:40:30 -0600

    deleted:    src/app/environments/config.ts

--- src/app/environments/config.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    baseUrl: 'https://backend-filo.vercel.app', //API
-    recaptchaSiteKey: '6Lck9V8qAAAAAEnjZRJxMKa27R1P_GWppjLuxmbG', //  clave de reCAPTCHA
-};
