dfa72a3df431c4a6bbcb0c232817c6ec58add890
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-10-20 00:24:12 -0600

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.config.ts 	new file:   src/app/components/header/header.component.css 	new file:   src/app/components/header/header.component.html 	new file:   src/app/components/header/header.component.ts 	new file:   src/app/components/services/account/account.models.ts 	new file:   src/app/components/services/account/account.service.spec.ts 	new file:   src/app/components/services/account/account.service.ts 	new file:   src/app/components/services/auth/auth.models.ts 	new file:   src/app/components/services/auth/auth.service.spec.ts 	new file:   src/app/components/services/auth/auth.service.ts 	new file:   src/app/components/services/csrf/csrf.service.spec.ts new file:   src/app/components/services/csrf/csrf.service.ts 	new file:   src/app/components/services/password/password.service.spec.ts 	new file:   src/app/components/services/password/password.service.ts 	new file:   src/app/components/services/theme.service.spec.ts 	new file:   src/app/components/services/theme.service.ts 	new file:   src/app/components/services/user/user.models.ts new file:   src/app/components/services/user/user.service.spec.ts 	new file:   src/app/components/services/user/user.service.ts 	new file:   src/app/components/shared/footer/footer.component.html 	new file:   src/app/components/shared/footer/footer.component.scss new file:   src/app/components/shared/footer/footer.component.spec.ts 	new file:   src/app/components/shared/footer/footer.component.ts 	new file:   src/app/components/shared/header/header.component.html 	new file:   src/app/components/shared/header/header.component.scss 	new file:   src/app/components/shared/header/header.component.spec.ts 	new file:   src/app/components/shared/header/header.component.ts 	modified:   src/index.html 	modified:   src/styles.css

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,MatSlideToggleModule
+  ]
+})
+export class HeaderComponent {
+  isMenuOpen = false; // Variable para controlar el menú lateral
+  isDarkTheme = false; // Variable para controlar el tema
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    const body = document.body;
+    if (this.isDarkTheme) {
+      body.classList.add('dark-theme');
+    } else {
+      body.classList.remove('dark-theme');
+    }
+  }
+}
---
+++ src/app/components/services/user/user.models.ts
@@ -0,0 +1,9 @@
+export interface Usuario {
+    id: number;
+    nombre: string;
+    apellido_paterno: string;
+    apellido_materno: string;
+    email: string;
+    telefono: string;
+    rol_id: number;
+}
