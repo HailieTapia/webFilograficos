6a21f72439b50a8658b232c28aeb917de4df68ad
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-10-20 21:14:33 -0600

    modified:   src/app/app.routes.ts 	modified:   src/app/components/header/header.component.html 	new file:   src/app/components/home/home.component.css 	new file:   src/app/components/home/home.component.html 	new file:   src/app/components/home/home.component.ts 	modified:   src/app/components/public/loginn/loginn.component.css 	modified:   src/app/components/public/loginn/loginn.component.html 	modified:   src/app/components/public/register/register.component.css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginnComponent } from './components/public/loginn/loginn.component'; 
 import { RegisterComponent } from './components/public/register/register.component'; 
+import { HomeComponent } from './components/home/home.component'; 
 
 export const routes: Routes = [
     { path: 'login', component: LoginnComponent },
     { path: 'registro', component: RegisterComponent },
+    { path: 'home', component: HomeComponent },
 ];
