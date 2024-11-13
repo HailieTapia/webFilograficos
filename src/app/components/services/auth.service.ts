3d82efaa716b691388b5270d035f06cab6b68da4
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-11-13 11:28:06 -0600

    modified:   src/app/components/services/auth.service.ts

--- src/app/components/services/auth.service.ts
+++ src/app/components/services/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, BehaviorSubject, of } from 'rxjs';
 import { environment } from '../../environments/config';
-import { tap } from 'rxjs/operators';
-import { CsrfService } from '../services/csrf.service';
+import { tap, switchMap } from 'rxjs/operators';
+import { CsrfService } from './csrf.service';  // Asegúrate de importar el servicio CSRF
 
 @Injectable({
   providedIn: 'root'
@@ -15,35 +15,39 @@ export class AuthService {
 
   constructor(private http: HttpClient, private csrfService: CsrfService) {
     const storedUser = localStorage.getItem('currentUser');
-    if (storedUser) {
-      this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(storedUser));
-    } else {
-      this.currentUserSubject = new BehaviorSubject<any>(null);
-    }
+    this.currentUserSubject = storedUser ? new BehaviorSubject<any>(JSON.parse(storedUser)) : new BehaviorSubject<any>(null);
     this.currentUser = this.currentUserSubject.asObservable();
   }
-
-  // Login con CSRF
+  // Login con CSRF LSITO 
   login(credentials: { email: string; password: string; recaptchaToken: string }): Observable<any> {
-    return this.csrfService.getCsrfToken().pipe(
-      tap(csrfToken => {
-        // Después de obtener el token CSRF, hacer la solicitud de login
-        return this.csrfService.postWithCsrf(`${this.apiUrl}/auth/login`, credentials, csrfToken).pipe(
+    return this.csrfService.getCsrfToken().pipe( // Obtener el token CSRF
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken); // Agregar el token CSRF al encabezado
+        return this.http.post(`${this.apiUrl}/auth/login`, credentials, {
+          headers, // Incluir los encabezados con el token CSRF
+          withCredentials: true // Asegura que se envíen las cookies (como la sesión) con la solicitud
+        }).pipe(
           tap((response: any) => {
             console.log('Respuesta del login:', response);
-            this.currentUserSubject.next(response);
-            localStorage.setItem('currentUser', JSON.stringify(response));
+            if (response && response.verified) {
+              this.currentUserSubject.next(response); // Actualizar el estado del usuario
+              localStorage.setItem('currentUser', JSON.stringify(response)); // Guardar la información del usuario en localStorage
+            } else {
+              // Si la verificación falla, puedes redirigir o hacer algo con la respuesta
+              console.log('Error en la verificación del login');
+            }
           })
         );
       })
     );
   }
-
-  // Método para registrar un nuevo usuario con CSRF
+  
+  // Método para registrar un nuevo usuario LISTO
   register(userData: any): Observable<any> {
-    return this.csrfService.getCsrfToken().pipe(
-      tap(csrfToken => {
-        return this.csrfService.postWithCsrf(`${this.apiUrl}/auth/register/`, userData, csrfToken);
+    return this.csrfService.getCsrfToken().pipe( // Obtener el token CSRF
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken); // Agregar el token CSRF al encabezado
+        return this.http.post(`${this.apiUrl}/auth/register/`, userData, { headers,  withCredentials: true });
       })
     );
   }
@@ -88,31 +92,34 @@ export class AuthService {
     return currentUser ? currentUser.userId : null;
   }
 
-  // Metodo para cerrar sesión
+  // Metodo para cerrar sesión LISTO
   logout(): Observable<any> {
-    return this.http.post(`${this.apiUrl}/auth/logout`, {}, { withCredentials: true }).pipe(
-      tap(() => {
-        localStorage.removeItem('currentUser');
-        this.currentUserSubject.next(null);
+    return this.csrfService.getCsrfToken().pipe( // Obtener el token CSRF
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken); // Agregar el token CSRF al encabezado
+        return this.http.post(`${this.apiUrl}/auth/logout`, {}, {
+          headers, // Incluir el token CSRF en los encabezados
+          withCredentials: true // Asegura que se envíen las cookies (como la sesión) con la solicitud
+        }).pipe(
+          tap(() => {
+            // Al cerrar sesión, eliminamos la información del usuario
+            localStorage.removeItem('currentUser');
+            this.currentUserSubject.next(null); // Reseteamos el estado del usuario
+          })
+        );
       })
     );
   }
-
+  
   // Método para obtener el perfil del usuario
   getProfile(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/users/profile`, { withCredentials: true });
+    return this.csrfService.getCsrfToken().pipe(
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken);
+        return this.http.get(`${this.apiUrl}/users/profile`, { headers, withCredentials: true });
+      })
+    );
   }
-
-  // Actualización del perfil del usuario (nombre, dirección, teléfono)
-  updateProfile(data: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/users/profile`, data, { withCredentials: true });
-  }
-
-  // Actualizar solo la dirección del usuario
-  updateUserProfile(direccion: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/users/profile/change-address`, { direccion }, { withCredentials: true });
-  }
-
   // Método para eliminar la cuenta del usuario
   deleteAccount(): Observable<any> {
     return this.http.delete(`${this.apiUrl}/users/delete-account`, { withCredentials: true })
@@ -121,6 +128,36 @@ export class AuthService {
           localStorage.clear();
         })
       );
+  }
+
+  // Actualización del perfil del usuario (nombre, dirección, teléfono)
+  updateProfile(data: any): Observable<any> {
+    return this.csrfService.getCsrfToken().pipe(
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken);
+        return this.http.put(`${this.apiUrl}/users/profile`, data, { headers, withCredentials: true });
+      })
+    );
+  }
+
+  // Actualizar solo la dirección del usuario
+  updateUserProfile(direccion: any): Observable<any> {
+    return this.csrfService.getCsrfToken().pipe(
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken);
+        return this.http.put(`${this.apiUrl}/users/profile/change-address`, { direccion }, { headers, withCredentials: true });
+      })
+    );
+  }
+
+  // Método para eliminar la cuenta del usuario
+  deleteMyAccount(): Observable<any> {
+    return this.csrfService.getCsrfToken().pipe(
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken);
+        return this.http.delete(`${this.apiUrl}/users/delete-account`, { headers, withCredentials: true });
+      })
+    );
   }
 
   // Desactivar la cuenta de usuario
