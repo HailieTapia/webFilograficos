4947daa895f79cab3cd428d4189c6d48a20f5d03
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-11-13 12:10:50 -0600

    modified:   src/app/components/services/auth.service.ts

--- src/app/components/services/auth.service.ts
+++ src/app/components/services/auth.service.ts
@@ -20,27 +20,20 @@ export class AuthService {
   }
   // Login con CSRF LSITO 
   login(credentials: { email: string; password: string; recaptchaToken: string }): Observable<any> {
-    return this.csrfService.getCsrfToken().pipe( // Obtener el token CSRF
+    return this.csrfService.getCsrfToken().pipe(
       switchMap(csrfToken => {
-        const headers = new HttpHeaders().set('x-csrf-token', csrfToken); // Agregar el token CSRF al encabezado
-        return this.http.post(`${this.apiUrl}/auth/login`, credentials, {
-          headers, // Incluir los encabezados con el token CSRF
-          withCredentials: true // Asegura que se envíen las cookies (como la sesión) con la solicitud
-        }).pipe(
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken);
+        return this.http.post(`${this.apiUrl}/auth/login`, credentials, { headers, withCredentials: true }).pipe(
           tap((response: any) => {
-            console.log('Respuesta del login:', response);
-            if (response && response.verified) {
-              this.currentUserSubject.next(response); // Actualizar el estado del usuario
-              localStorage.setItem('currentUser', JSON.stringify(response)); // Guardar la información del usuario en localStorage
-            } else {
-              // Si la verificación falla, puedes redirigir o hacer algo con la respuesta
-              console.log('Error en la verificación del login');
-            }
+            console.log('respuesta ', response); // Agrega esta línea para verificar la respuesta
+            this.currentUserSubject.next(response);
+            localStorage.setItem('currentUser', JSON.stringify(response));
           })
         );
       })
     );
   }
+  
   
   // Método para registrar un nuevo usuario LISTO
   register(userData: any): Observable<any> {
@@ -56,29 +49,42 @@ export class AuthService {
   // Iniciar el proceso de recuperación de contraseña con CSRF
   recu(credentials: any): Observable<any> {
     return this.csrfService.getCsrfToken().pipe(
-      tap(csrfToken => {
-        return this.csrfService.postWithCsrf(`${this.apiUrl}/auth/initiate-password-recovery`, credentials, csrfToken);
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken);
+        return this.http.post(`${this.apiUrl}/auth/initiate-password-recovery`, credentials, {
+          headers,
+          withCredentials: true
+        });
       })
     );
   }
-
+  
   // Verificar el código OTP con CSRF
   verOTP(credentials: any): Observable<any> {
     return this.csrfService.getCsrfToken().pipe(
-      tap(csrfToken => {
-        return this.csrfService.postWithCsrf(`${this.apiUrl}/auth/verify-otp`, credentials, csrfToken);
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken);
+        return this.http.post(`${this.apiUrl}/auth/verify-otp`, credentials, {
+          headers,
+          withCredentials: true
+        });
       })
     );
   }
-
+  
   // Reestablecer la contraseña con CSRF
   resContra(credentials: any): Observable<any> {
     return this.csrfService.getCsrfToken().pipe(
-      tap(csrfToken => {
-        return this.csrfService.postWithCsrf(`${this.apiUrl}/auth/reset-password`, credentials, csrfToken);
+      switchMap(csrfToken => {
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken);
+        return this.http.post(`${this.apiUrl}/auth/reset-password`, credentials, {
+          headers,
+          withCredentials: true
+        });
       })
     );
   }
+  
 
   // Método para obtener el tipo de usuario actual como un Observable
   getTipoUsuario(): Observable<string | null> {
@@ -94,22 +100,19 @@ export class AuthService {
 
   // Metodo para cerrar sesión LISTO
   logout(): Observable<any> {
-    return this.csrfService.getCsrfToken().pipe( // Obtener el token CSRF
+    return this.csrfService.getCsrfToken().pipe(
       switchMap(csrfToken => {
-        const headers = new HttpHeaders().set('x-csrf-token', csrfToken); // Agregar el token CSRF al encabezado
-        return this.http.post(`${this.apiUrl}/auth/logout`, {}, {
-          headers, // Incluir el token CSRF en los encabezados
-          withCredentials: true // Asegura que se envíen las cookies (como la sesión) con la solicitud
-        }).pipe(
+        const headers = new HttpHeaders().set('x-csrf-token', csrfToken);
+        return this.http.post(`${this.apiUrl}/auth/logout`, {}, { headers, withCredentials: true }).pipe(
           tap(() => {
-            // Al cerrar sesión, eliminamos la información del usuario
             localStorage.removeItem('currentUser');
-            this.currentUserSubject.next(null); // Reseteamos el estado del usuario
+            this.currentUserSubject.next(null);
           })
         );
       })
     );
   }
+  
   
   // Método para obtener el perfil del usuario
   getProfile(): Observable<any> {
