0976f7e52c57e02f559be3cefabbabd55d1575c9
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-11-14 11:08:18 -0600

    modified:   src/app/components/public/register/register.component.css 	modified:   src/app/components/public/register/register.component.html 	modified:   src/app/components/public/register/register.component.ts

--- src/app/components/public/register/register.component.ts
+++ src/app/components/public/register/register.component.ts
@@ -5,145 +5,190 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { PwnedService } from '../../services/pwned.service';
-import { trigger, transition, style, animate } from '@angular/animations';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatChipsModule } from '@angular/material/chips';
+import { Observable } from 'rxjs';
+import { AbstractControl } from '@angular/forms';
+import { MatProgressBarModule } from '@angular/material/progress-bar'; 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSnackBarModule,
-  ],
+  imports: [MatProgressBarModule,MatChipsModule,MatProgressSpinnerModule,CommonModule,ReactiveFormsModule,MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule,MatIconModule],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
-  animations: [
-    trigger('transitionMessages', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('500ms ease-in', style({ opacity: 1 }))
-      ]),
-      transition(':leave', [
-        animate('500ms ease-out', style({ opacity: 0 }))
-      ])
-    ])
-  ]
+  styleUrls: ['./register.component.css', '../../estilos/spinner.css', '../../estilos/snackbar.css'],
 })
 export class RegisterComponent {
   registerForm: FormGroup;
   hide: boolean = true;
-  errorMessage: string | null = null;
-  successMessage: string | null = null;
-  errorMessage1: string | null = null;
-  successMessage1: string | null = null;
+  hideConfirm: boolean = true;
+  isLoading: boolean = false;
+  passwordStrength: string = '';
+  passwordStrengthValue: number = 0;
+  passwordStrengthColor: string = 'warn';
 
-  constructor(private pwnedService: PwnedService, private fb: FormBuilder, private snackBar: MatSnackBar, private authService: AuthService, private router: Router) {
+  constructor(
+    private pwnedService: PwnedService,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.registerForm = this.fb.group({
       nombre: ['', [Validators.required, Validators.pattern(/^[a-zA-ZáéíóúÁÉÍÓÚñÑäöüÄÖÜ\s]+$/), Validators.minLength(3), Validators.maxLength(15)]],
-      email: ['', [Validators.required, Validators.email, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]],
+      email: ['', [Validators.required, Validators.email]],
       telefono: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(8)], [this.passwordStrengthValidator.bind(this)]],
       confirmPassword: ['', [Validators.required]],
       tipo_usuario: ['cliente'],
-      mfa_activado: [false]
+      mfa_activado: [false],
     }, { validators: this.passwordMatchValidator });
-     // Suscribirse a los cambios en los campos de contraseña y confirmación
-     this.registerForm.get('password')?.valueChanges.subscribe(() => {
-      this.registerForm.updateValueAndValidity(); // Para re-evaluar el formulario
-    });
-    
-    this.registerForm.get('confirmPassword')?.valueChanges.subscribe(() => {
-      this.registerForm.updateValueAndValidity(); // Para re-evaluar el formulario
-    });
   }
 
   get f() {
     return this.registerForm.controls;
   }
-  showAlert(message: string | null) {
-    if (message) { // Verifica que no sea null o undefined
-      this.snackBar.open(message, 'Cerrar', {
-        duration: 3000,
-      });
-    }
-  }
-  // Valida que las contraseñas coincidan
   passwordMatchValidator(form: FormGroup) {
-    return form.get('password')?.value === form.get('confirmPassword')?.value ? null : { mismatch: true };
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password && confirmPassword && password !== confirmPassword ? { mismatch: true } : null;
   }
 
-  // Método para validar la contraseña
-  validatePasswordStrength(password: string) {
-    this.pwnedService.checkPassword(password).subscribe({
-      next: (data) => {
-        const lines = data.split('\n');
-        const passwordHashSuffix = this.pwnedService.sha1(password).substring(5).toUpperCase();
+  
+  checkPasswordStrength(password: string): string {
+    let strength = '';
+    const lengthCondition = password.length >= 8;
+    const hasNumber = /[0-9]/.test(password);
+    const hasLower = /[a-z]/.test(password);
+    const hasUpper = /[A-Z]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
-        const match = lines.find(line => line.startsWith(passwordHashSuffix));
-        if (match) {
-          this.errorMessage1 = `Tu contraseña ha sido comprometida ${match.split(':')[1]} veces. Por favor, usa una contraseña más segura.`;
-          this.hideMessageAfterDelay('errorMessage1');
-        } else {
-          this.successMessage1 = 'Tu contraseña es segura.';
-           this.hideMessageAfterDelay('successMessage1');
-          this.onRegister(); // Llama al método para continuar con el registro
-        }
-      },
-      error: () => {
-        this.errorMessage1 = 'Error al verificar la seguridad de la contraseña.';
-        this.hideMessageAfterDelay('errorMessage1');
-      }
-    });
-  }
-   // Método para ocultar el mensaje después de 2 segundos
-   hideMessageAfterDelay(messageType: 'errorMessage1' | 'successMessage1') {
-    setTimeout(() => {
-      if (messageType === 'errorMessage1') {
-        this.errorMessage1 = null;
-      } else if (messageType === 'successMessage1') {
-        this.successMessage1 = null;
-      }
-    }, 3000);
-  }
-
-  // Método para continuar con el registro una vez validada la contraseña
-  onRegister() {
-    const { nombre, email, telefono, password, tipo_usuario, mfa_activado } = this.registerForm.value;
-
-    this.authService.register({ nombre, email, telefono, password, tipo_usuario, mfa_activado }).subscribe({
-      next: () => {
-        this.successMessage = 'Registro exitoso';
-        this.showAlert(this.successMessage);
-        setTimeout(() => this.router.navigate(['/login']), 2000);
-      },
-      error: (err) => {
-        this.errorMessage = err.error?.message || 'Error en el registro';
-        this.showAlert(this.errorMessage);
-      }
-    });
-  }
-
-  onSubmit() {
-    this.errorMessage = null;
-    this.successMessage = null;
-
-    if (this.registerForm.valid) {
-      const password = this.registerForm.get('password')?.value;
-      // Llamar al servicio para validar si la contraseña ha sido comprometida
-      this.validatePasswordStrength(password);
+    if (lengthCondition && hasNumber && hasLower && hasUpper && hasSpecialChar) {
+      strength = 'Fuerte';
+      this.passwordStrengthValue = 100;
+      this.passwordStrengthColor = 'primary';
+    } else if (lengthCondition && hasNumber && (hasLower || hasUpper)) {
+      strength = 'Moderada';
+      this.passwordStrengthValue = 70;
+      this.passwordStrengthColor = 'accent';
+    } else if (lengthCondition) {
+      strength = 'Débil';
+      this.passwordStrengthValue = 40;
+      this.passwordStrengthColor = 'warn';
     } else {
-      this.errorMessage = 'Por favor completa todos los campos requeridos.';
-      this.showAlert(this.errorMessage);
+      strength = 'Muy débil';
+      this.passwordStrengthValue = 10;
+      this.passwordStrengthColor = 'warn';
     }
+
+    return strength;
   }
-}
+
+  onPasswordChange(password: string) {
+    this.passwordStrength = this.checkPasswordStrength(password); 
+  }
+
+  // Función para verificar si la contraseña está comprometida
+  validatePasswordStrength(password: string): Observable<string> {
+    return new Observable((observer) => {
+      this.pwnedService.checkPassword(password).subscribe({
+        next: (data) => {
+          const lines = data.split('\n');
+          const passwordHashSuffix = this.pwnedService.sha1(password).substring(5).toUpperCase();
+          const match = lines.find(line => line.startsWith(passwordHashSuffix));
+
+          if (match) {
+            observer.next('comprometida');
+          } else {
+            observer.next('segura');
+          }
+        },
+        error: () => {
+          observer.next('error');
+        }
+      });
+    });
+  }
+
+  passwordStrengthValidator(control: AbstractControl): Observable<{ [key: string]: boolean } | null> {
+    return new Observable((observer) => {
+      const password = control.value;
+      this.pwnedService.checkPassword(password).subscribe({
+        next: (data) => {
+          const lines = data.split('\n');
+          const passwordHashSuffix = this.pwnedService.sha1(password).substring(5).toUpperCase();
+          const match = lines.find(line => line.startsWith(passwordHashSuffix));
+  
+          if (match) {
+            observer.next({ comprometida: true });
+          } else {
+            observer.next(null);
+          }
+        },
+        error: () => {
+          observer.next(null);
+        }
+      });
+    });
+  }
+  onRegister() {
+    this.isLoading = true;
+  
+    if (this.registerForm.invalid) {
+      // Detener el registro si el formulario no es válido (es decir, si las contraseñas no coinciden)
+      this.snackBar.open('Por favor, completa el formulario correctamente.', 'Cerrar', {
+        duration: 3000
+      });
+      this.isLoading = false;
+      return; 
+    }
+  
+    if (this.passwordStrengthValue !== 100) {
+      this.snackBar.open('Tu contraseña debe ser 100% de fortaleza.', 'Cerrar', {
+        duration: 3000
+      });
+      this.isLoading = false;
+      return; 
+    }
+  
+    const { nombre, email, telefono, password, tipo_usuario, mfa_activado } = this.registerForm.value;
+  
+    this.validatePasswordStrength(password).subscribe((status) => {
+      if (status === 'comprometida') {
+        this.registerForm.get('password')?.setErrors({ comprometida: true });
+  
+        this.snackBar.open('Tu contraseña ha sido comprometida, usa una contraseña más segura.', 'Cerrar', {
+          duration: 3000
+        });
+        this.isLoading = false;
+      } else if (status === 'segura') {
+        this.registerForm.get('password')?.setErrors(null);
+  
+        this.authService.register({ nombre, email, telefono, password, tipo_usuario, mfa_activado }).subscribe({
+          next: () => {
+            this.isLoading = false;
+            this.snackBar.open('Registro exitoso', 'Cerrar', {
+              duration: 3000
+            }).afterDismissed().subscribe(() => {
+              this.router.navigate(['/login']);
+            });
+          },
+          error: (error) => {
+            const errorMessage = error?.error?.message || 'Error en el registro';
+            this.snackBar.open(errorMessage, 'Cerrar', { duration: 3000 });
+            this.isLoading = false;
+          }
+        });
+      } else {
+        this.snackBar.open('Error al verificar la seguridad de la contraseña.', 'Cerrar', {
+          duration: 3000
+        });
+        this.isLoading = false;
+      }
+    });
+  }
+}  
