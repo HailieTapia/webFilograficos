09b9693e928b551e96671c0c7f4cc18e9cc70606
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-11-23 22:28:13 -0600

    modified:   src/app/components/public/register/register.component.css 	modified:   src/app/components/public/register/register.component.html 	modified:   src/app/components/public/register/register.component.ts

--- src/app/components/public/register/register.component.ts
+++ src/app/components/public/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl,ValidationErrors,FormControl,ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -13,7 +13,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { Observable, of,catchError } from 'rxjs';
+import { Observable, of, catchError } from 'rxjs';
 import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-register',
@@ -44,101 +44,82 @@ export class RegisterComponent {
       nombre: ['', [Validators.required, Validators.pattern(/^(?=.*[a-zA-ZáéíóúÁÉÍÓÚñÑäöüÄÖÜ])[a-zA-ZáéíóúÁÉÍÓÚñÑäöüÄÖÜ\s]*$/), Validators.minLength(3), Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
       telefono: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
-      password: ['', [Validators.required, Validators.minLength(8), this.passwordStrengthValidator.bind(this)], [this.checkPasswordCompromised.bind(this)]],
+      password: ['', [Validators.required, this.passwordStrengthValidator.bind(this),], [this.checkPasswordCompromised.bind(this)],],
       confirmPassword: ['', [Validators.required]],
       tipo_usuario: ['cliente'],
       mfa_activado: [false],
-    }, { validators: this.passwordMatchValidator });    
-  }
-  // Validador asincrónico para verificar si la contraseña está comprometida
-checkPasswordCompromised(control: AbstractControl): Observable<ValidationErrors | null> {
-  const password = control.value;
-  if (!password) {
-    return of(null); // No hay error si el campo está vacío
-  }
-
-  return this.validatePasswordStrength(password).pipe(
-    map((status: string) => {
-      if (status === 'comprometida') {
-        control.setErrors({ compromised: true });
-        return { compromised: true };
-      }
-      return null;
-    }),
-    catchError(() => {
-      control.setErrors({ compromisedError: true }); // Manejo de errores
-      return of(null);
-    })
-  );
-}
-  // Función para verificar si la contraseña está comprometida
-  validatePasswordStrength(password: string): Observable<string> {
-    return new Observable((observer) => {
-      this.pwnedService.checkPassword(password).subscribe({
-        next: (data) => {
-          const lines = data.split('\n');
-          const passwordHashSuffix = this.pwnedService.sha1(password).substring(5).toUpperCase();
-          const match = lines.find(line => line.startsWith(passwordHashSuffix));
-          if (match) {
-            observer.next('comprometida');
-          } else {
-            observer.next('segura');
-          }
-        },
-        error: () => {
-          observer.next('error');
-        }
-      });
-    });
+    },
+      { validators: this.passwordMatchValidator }
+    );
   }
 
   // Validador personalizado para la fortaleza de la contraseña
   passwordStrengthValidator(control: FormControl): { [key: string]: any } | null {
     const password = control.value || '';
-    this.checkPasswordStrength(password); 
 
+    // Si ya hay un error de 'minlength', no agregar más errores
+    if (control.hasError('minlength')) {
+      return null;
+    }
+
+    this.checkPasswordStrength(password);
     if (this.passwordStrengthValue < 100) {
-      return { weakPassword: true }; 
+      return { weakPassword: true };
     }
     return null;
   }
-  //Barra progreso
+
+  // Validador asincrónico para verificar si la contraseña está comprometida
+  checkPasswordCompromised(control: AbstractControl): Observable<ValidationErrors | null> {
+    const password = control.value;
+    if (!password) return of(null);
+
+    return this.pwnedService.checkPassword(password).pipe(
+      map((data) => {
+        const lines = data.split('\n');
+        const hashSuffix = this.pwnedService.sha1(password).substring(5).toUpperCase();
+        const isCompromised = lines.some((line) => line.startsWith(hashSuffix));
+
+        if (isCompromised) {
+          this.setPasswordStrength('Comprometida', 80, 'warn');
+          return { compromised: true };
+        }
+        return null;
+      }),
+      catchError(() => of(null))
+    );
+  }
+
+  // Método para calcular la fortaleza de la contraseña
   checkPasswordStrength(password: string): void {
     if (!password) {
-      this.passwordStrength = 'Muy débil';
-      this.passwordStrengthValue = 0;
-      this.passwordStrengthColor = 'warn';
+      this.setPasswordStrength('Muy débil', 0, 'warn');
       return;
     }
-  
-    let strength = '';
+
     const lengthCondition = password.length >= 8;
     const hasNumber = /[0-9]/.test(password);
     const hasLower = /[a-z]/.test(password);
     const hasUpper = /[A-Z]/.test(password);
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-  
+
     if (lengthCondition && hasNumber && hasLower && hasUpper && hasSpecialChar) {
-      strength = 'Fuerte';
-      this.passwordStrengthValue = 100;
-      this.passwordStrengthColor = 'primary';
+      this.setPasswordStrength('Fuerte', 100, 'primary');
     } else if (lengthCondition && hasNumber && (hasLower || hasUpper)) {
-      strength = 'Moderada';
-      this.passwordStrengthValue = 70;
-      this.passwordStrengthColor = 'accent';
+      this.setPasswordStrength('Moderada', 60, 'accent');
     } else if (lengthCondition) {
-      strength = 'Débil';
-      this.passwordStrengthValue = 40;
-      this.passwordStrengthColor = 'warn';
+      this.setPasswordStrength('Débil', 40, 'warn');
     } else {
-      strength = 'Muy débil';
-      this.passwordStrengthValue = 10;
-      this.passwordStrengthColor = 'warn';
+      this.setPasswordStrength('Muy débil', 10, 'warn');
     }
-  
-    this.passwordStrength = strength;
   }
-  
+
+  setPasswordStrength(strength: string, value: number, color: string): void {
+    this.passwordStrength = strength;
+    this.passwordStrengthValue = value;
+    this.passwordStrengthColor = color;
+  }
+
   // Validador personalizado para verificar que las contraseñas coincidan
   passwordMatchValidator(form: FormGroup) {
     const password = form.get('password')?.value || '';
@@ -165,24 +146,21 @@ checkPasswordCompromised(control: AbstractControl): Observable<ValidationErrors 
       this.authService.register(userData).subscribe({
         next: () => {
           this.isLoading = false;
-          this.snackBar.open('Registro exitoso', 'Cerrar', {
-            duration: 3000
-          }).afterDismissed().subscribe(() => {
-            this.router.navigate(['/login']);
-          });
+          this.snackBar.open('Registro exitoso', 'Cerrar', { duration: 3000 });
+          this.router.navigate(['/login']);
         },
         error: (error) => {
           const errorMessage = error?.error?.message || 'Error en el registro';
           this.snackBar.open(errorMessage, 'Cerrar', { duration: 3000 });
           this.isLoading = false;
-        }
+        },
       });
     } else {
-      this.snackBar.open('Por favor completa todos los campos correctamente.', 'Cerrar', {
-        duration: 3000
-      });
-      this.isLoading = false;
+      this.snackBar.open(
+        'Por favor completa todos los campos correctamente.',
+        'Cerrar',
+        { duration: 3000 }
+      );
     }
   }
 }
-
