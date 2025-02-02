3b800724feb25ae20dc31d8044751462a1740e04
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2025-02-02 17:39:59 -0600

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.config.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule, HeaderComponent, FooterComponent] 
+  imports: [BreadcrumbsComponent,RouterModule, HeaderComponent, FooterComponent] 
 })
 export class AppComponent { }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes'; 
+import { routes } from './app.routes';
+import { errorHandlerInterceptor } from './components/services/error-handler.service'; // Importa la función interceptor
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withFetch()),  
-    provideRouter(routes),         
+    provideHttpClient(withInterceptors([errorHandlerInterceptor])), // Usa la función interceptor en lugar de la clase
+    provideRouter(routes),
   ]
 };
-
-
